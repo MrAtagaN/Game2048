055e404609e35c0b0a684a31692634c1fdb59a5f
AtagaN <atagan@rambler.ru>
2018-05-08 01:35:12 +0400

    Initial commit

---
+++ src/Move.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Created by AtagaN on 04.01.2018.
+ */
+@FunctionalInterface
+public interface Move {
+    public void move();
+}
---
+++ src/MoveEfficiency.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Created by AtagaN on 04.01.2018.
+ */
+public class MoveEfficiency implements Comparable<MoveEfficiency>{
+    private int numberOfEmptyTiles;
+    private int score;
+    private Move move;
+
+
+    public MoveEfficiency(int numberOfEmptyTiles, int score, Move move){
+        this.numberOfEmptyTiles=numberOfEmptyTiles;
+        this.move = move;
+        this.score = score;
+    }
+
+    public Move getMove() {
+        return move;
+    }
+
+    @Override
+    public int compareTo(MoveEfficiency o) {
+        if( this.numberOfEmptyTiles > o.numberOfEmptyTiles){
+            return 1;
+        }
+        if(this.numberOfEmptyTiles < o.numberOfEmptyTiles){
+            return -1;
+        }
+        if(this.numberOfEmptyTiles == o.numberOfEmptyTiles){
+            if(this.score>o.score){
+                return 1;
+            }
+            if(this.score<o.score){
+                return -1;
+            }
+            if(this.score<o.score){
+                return 0;
+            }
+        }
+       return 0;
+    }
+}
